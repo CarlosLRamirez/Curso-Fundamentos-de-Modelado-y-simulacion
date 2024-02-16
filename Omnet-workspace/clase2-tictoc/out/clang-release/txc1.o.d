8124d1edd6feb883f4d78397b13208b5c8f5fc49
Carlos L Ramirez <carloslrm@gmail.com>
2024-02-16 06:39:49 -0600

    primer commit

---
+++ Omnet-workspace/clase2-tictoc/out/clang-release/txc1.o.d
@@ -0,0 +1,4 @@
+out/clang-release//txc1.o: txc1.cc \
+  /Users/carlos.ramirez/omnetpp-6.0.2/include/omnetpp.h
+
+/Users/carlos.ramirez/omnetpp-6.0.2/include/omnetpp.h:
---
+++ Omnet-workspace/clase2-tictoc/out/clang-release/txc3.o.d
@@ -0,0 +1,4 @@
+out/clang-release//txc3.o: txc3.cc \
+  /Users/carlos.ramirez/omnetpp-6.0.2/include/omnetpp.h
+
+/Users/carlos.ramirez/omnetpp-6.0.2/include/omnetpp.h:
