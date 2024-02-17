9d5f4e9217a8bc6f6aa42229b9ba2bd76f17f857
Carlos L Ramirez <carloslrm@gmail.com>
2024-02-17 13:17:12 -0600

    clase 3 ready

---
+++ omnet-workspace/clase3-tictoc2/out/clang-release/tictoc18_m.o.d
@@ -0,0 +1,6 @@
+out/clang-release//tictoc18_m.o: tictoc18_m.cc tictoc18_m.h \
+  /Users/carlos.ramirez/omnetpp-6.0.2/include/omnetpp.h
+
+tictoc18_m.h:
+
+/Users/carlos.ramirez/omnetpp-6.0.2/include/omnetpp.h:
---
+++ omnet-workspace/clase3-tictoc2/out/clang-release/txc18.o.d
@@ -0,0 +1,6 @@
+out/clang-release//txc18.o: txc18.cc \
+  /Users/carlos.ramirez/omnetpp-6.0.2/include/omnetpp.h tictoc18_m.h
+
+/Users/carlos.ramirez/omnetpp-6.0.2/include/omnetpp.h:
+
+tictoc18_m.h:
