098c9cf6511806023e5b6115f41f1ce63256a997
Carlos L Ramirez <carloslrm@gmail.com>
2024-02-26 06:22:39 -0600

    folder eliminado

--- omnet-workspace/ejercicio_dist_probabilidad/out/clang-debug/minodo.o.d
+++
@@ -1,4 +0,0 @@
-out/clang-debug//minodo.o: minodo.cc \
-  /Users/carlos.ramirez/omnetpp-6.0.2/include/omnetpp.h
-
-/Users/carlos.ramirez/omnetpp-6.0.2/include/omnetpp.h:
--- omnet-workspace/ejercicio_dist_probabilidad/out/clang-release/minodo.o.d
+++
@@ -1,4 +0,0 @@
-out/clang-release//minodo.o: minodo.cc \
-  /Users/carlos.ramirez/omnetpp-6.0.2/include/omnetpp.h
-
-/Users/carlos.ramirez/omnetpp-6.0.2/include/omnetpp.h:
